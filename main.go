e591cd0b8fe4f9d9d2bccef43b513447c6de2cab
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-26 17:58:51 +0300

    Add temp main file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/stscoundrel/troubleman/github"
 	"github.com/stscoundrel/troubleman/issues"
 )
@@ -8,4 +10,16 @@ import (
 func GetIssues(username string) []issues.Issue {
 	rawIssues, _ := github.GetIssues("stscoundrel", github.BASE_ISSUE_SEARCH_URL)
 	return issues.FromJson(rawIssues)
+}
+
+func main() {
+	issueList := GetIssues("stscoundrel")
+
+	for _, issue := range issueList {
+		fmt.Println(issue.Title)
+		fmt.Println(issue.Content)
+		fmt.Println(issue.Link)
+
+		fmt.Println("###########################")
+	}
 }
