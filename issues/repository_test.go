ea87f58e35e116caacd88d7ee9dc9beec71f1c58
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-07-02 09:50:30 +0300

    Sort repos by url

--- issues/repository_test.go
+++ issues/repository_test.go
@@ -9,34 +9,34 @@ import (
 func TestRepositoriesFromIssues(t *testing.T) {
 	issues := []Issue{
 		{
-			Title:          "Test issue 1",
-			Link:           "https://github.com/stscoundrel/test-repo/issues/1",
+			Title:          "Test issue 2",
+			Link:           "https://github.com/stscoundrel/cde-test-repo/issues/2",
 			Content:        "",
-			RepositoryLink: "https://api.github.com/stscoundrel/test-repo",
+			RepositoryLink: "https://api.github.com/stscoundrel/cde-test-repo",
 		},
 		{
-			Title:          "Test issue 2",
-			Link:           "https://github.com/stscoundrel/test-repo/issues/2",
+			Title:          "Test issue 1",
+			Link:           "https://github.com/stscoundrel/abc-test-repo/issues/1",
 			Content:        "",
-			RepositoryLink: "https://api.github.com/stscoundrel/test-repo2",
+			RepositoryLink: "https://api.github.com/stscoundrel/abc-test-repo",
 		},
 		{
 			Title:          "Test issue 3",
-			Link:           "https://github.com/stscoundrel/test-repo/issues/3",
+			Link:           "https://github.com/stscoundrel/abc-test-repo/issues/3",
 			Content:        "",
-			RepositoryLink: "https://api.github.com/stscoundrel/test-repo",
+			RepositoryLink: "https://api.github.com/stscoundrel/abc-test-repo",
 		},
 		{
 			Title:          "Test issue 4",
-			Link:           "https://github.com/stscoundrel/test-repo/issues/4",
+			Link:           "https://github.com/stscoundrel/cde-test-repo/issues/4",
 			Content:        "",
-			RepositoryLink: "https://api.github.com/stscoundrel/test-repo2",
+			RepositoryLink: "https://api.github.com/stscoundrel/cde-test-repo",
 		},
 		{
 			Title:          "Test issue 5",
-			Link:           "https://github.com/stscoundrel/test-repo/issues/5",
+			Link:           "https://github.com/stscoundrel/abc-test-repo/issues/5",
 			Content:        "",
-			RepositoryLink: "https://api.github.com/stscoundrel/test-repo",
+			RepositoryLink: "https://api.github.com/stscoundrel/abc-test-repo",
 		},
 	}
 
@@ -44,20 +44,20 @@ func TestRepositoriesFromIssues(t *testing.T) {
 
 	assert.Equal(t, len(result), 2)
 
-	assert.Equal(t, result[0].Title, "stscoundrel/test-repo")
+	assert.Equal(t, result[0].Title, "stscoundrel/abc-test-repo")
 	assert.Equal(t, result[0].Count, 3)
-	assert.Equal(t, result[0].Link, "https://github.com/stscoundrel/test-repo")
+	assert.Equal(t, result[0].Link, "https://github.com/stscoundrel/abc-test-repo")
 	assert.Equal(t, result[0].Issues, []Issue{
-		issues[0],
+		issues[1],
 		issues[2],
 		issues[4],
 	})
 
-	assert.Equal(t, result[1].Title, "stscoundrel/test-repo2")
+	assert.Equal(t, result[1].Title, "stscoundrel/cde-test-repo")
 	assert.Equal(t, result[1].Count, 2)
-	assert.Equal(t, result[1].Link, "https://github.com/stscoundrel/test-repo2")
+	assert.Equal(t, result[1].Link, "https://github.com/stscoundrel/cde-test-repo")
 	assert.Equal(t, result[1].Issues, []Issue{
-		issues[1],
+		issues[0],
 		issues[3],
 	})
 }
